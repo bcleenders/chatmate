8545ad12436ca79dcec5eae038084e87ed35692e
bcleenders <bcleenders@gmail.com>
2014-11-23 15:04:46 +0100

    Adds geo data again

--- chatmate.go
+++ chatmate.go
@@ -5,19 +5,22 @@ import (
 	"github.com/googollee/go-socket.io"
 	"log"
 	"net/http"
-	"strings"
-	"sync"
-	"time"
+    "sync"
+    "strings"
+    "time"
+    // "main"
 )
 
 // Should look like path
 const websocketRoom = "/chat"
 
 type initMsg struct {
-	username  string
-	latitude  float64
-	longitude float64
+	Username  string
+	Latitude  float64
+	Longitude float64
 }
+
+var rooms []Room = make([]Room, 64)
 
 func main() {
 	lastMessages := []string{}
@@ -41,20 +44,17 @@ func main() {
 		}
 		lmMutex.Unlock()
 
-		so.On("joined_message", func(message string) {
-			log.Println("w000t")
-			log.Println(message)
-
+        so.On("joined_message", func(message string) {
 			decoder := json.NewDecoder(strings.NewReader(message))
 			var msg initMsg
 			decoder.Decode(&msg)
 
 			log.Printf("joined_message %+v\n", msg)
 			res := map[string]interface{}{
-				"username":  msg.username,
+				"username":  msg.Username,
 				"dateTime":  time.Now().UTC().Format(time.RFC3339Nano),
-				"latitude":  msg.latitude,
-				"longitude": msg.longitude,
+				"latitude":  msg.Latitude,
+				"longitude": msg.Longitude,
 				"type":      "joined_message",
 			}
 			jsonRes, _ := json.Marshal(res)
@@ -79,20 +79,47 @@ func main() {
 			so.Emit("message", string(jsonRes))
 			so.BroadcastTo(websocketRoom, "message", string(jsonRes))
 		})
-		so.On("disconnection", func() {
-			log.Println("on disconnect", username)
-		})
-	})
-	sio.On("error", func(so socketio.Socket, err error) {
-		log.Println("error:", err)
-	})
+        so.On("new_group", func(message string) {
+            // Create the new group
+            messageChan := make(chan string)
+            newRoom := Room{
+                Name:"TestNaam", 
+                MessageChan:messageChan,
+            }
+            go newRoom.Run()
+        })
+        so.On("send_message", func(message string) {
+            log.Println("send_message from", username)
+            res := map[string]interface{}{
+                "username": username,
+                "message":  message,
+                "dateTime": time.Now().UTC().Format(time.RFC3339),
+                "type":     "message",
+            }
+            jsonRes, _ := json.Marshal(res)
+            lmMutex.Lock()
+            if len(lastMessages) == 100 {
+                lastMessages = lastMessages[1:100]
+            }
+            lastMessages = append(lastMessages, string(jsonRes))
+            lmMutex.Unlock()
+            so.Emit("message", string(jsonRes))
+            so.BroadcastTo(websocketRoom, "message", string(jsonRes))
+        })
+        so.On("disconnection", func() {
+            log.Println("on disconnect", username)
+        })
+    })
+    sio.On("error", func(so socketio.Socket, err error) {
+        log.Println("error:", err)
+    })
 
 	// Sets up the handlers and listen on port 8080
 	http.Handle("/socket.io/", sio)
 	// Serve CSS, Javascript...
-	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("./public/"))))
+	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("/home/damnyankee/public/"))))
 	// Serve index.html
-	http.Handle("/", http.FileServer(http.Dir("./public/")))
+	http.Handle("/", http.FileServer(http.Dir("/home/damnyankee/public/")))
 
 	port := ":1337"
 	log.Println("Server started on port", port)
